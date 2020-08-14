be66c1b64c8f1b9b67915f293b99d10c276069eb
Tatiana Inama <tatii.oxo@gmail.com>
2020-08-14 17:25:14 +0200

    Add HTML5-to-pdf task. Add resume PDF

---
+++ index.js
@@ -0,0 +1,34 @@
+const HTML5ToPDF = require("html5-to-pdf");
+const path = require("path");
+
+const run = async () => {
+  const html5ToPDF = new HTML5ToPDF({
+    inputPath: path.join(__dirname, "index.html"),
+    outputPath: path.join(__dirname, "tatiana-inama-resume.pdf"),
+    templatePath: path.join(__dirname, "/"),
+    include: [
+      path.join(__dirname, "styles", "styles.css"),
+    ],
+    renderDelay: 10000,
+    pdf: {
+      printBackground: true,
+      format: 'A3'
+    }
+  })
+
+  await html5ToPDF.start()
+  await html5ToPDF.build()
+  await html5ToPDF.close()
+}
+
+(async () => {
+  try {
+    console.log("converting to pdf...");
+    await run()
+    console.log("done!")
+  } catch (error) {
+    console.error(error)
+  } finally {
+    process.exit();
+  }
+})()
